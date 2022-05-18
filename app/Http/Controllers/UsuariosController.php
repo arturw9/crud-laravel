c7418d350f4e52ee4d412cb657e8299463e20a29
arturw9 <arturw9@hotmail.com>
2022-05-18 18:33:42 -0300

    add

---
+++ app/Http/Controllers/UsuariosController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+
+class UsuariosController extends Controller
+{
+    public function index(){
+        $usuarios = Usuario::get();
+
+        return view('usuarios.list', ['usuarios' => $usuarios]);
+    }
+}
