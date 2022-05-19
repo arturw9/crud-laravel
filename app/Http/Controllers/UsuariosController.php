fe9c69c40ac1f457a0b516b7915663999f6906bc
arturw9 <arturw9@hotmail.com>
2022-05-18 22:45:32 -0300

    Inserção dos metodos CRUD

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Usuario;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class UsuariosController extends Controller
 {
@@ -10,5 +11,29 @@ class UsuariosController extends Controller
         $usuarios = Usuario::get();
 
         return view('usuarios.list', ['usuarios' => $usuarios]);
+    }
+
+    public function new(){
+        return view('usuarios.form');
+    }
+    public function add(Request $request){
+        $usuario =  new Usuario;
+        $usuario = $usuario->create($request->all());
+        return Redirect::to('/usuarios');
+    }
+    public function edit($id){
+        $usuario = Usuario::findOrFail($id);
+        return view('usuarios.form',['usuario'=> $usuario]);
+    }
+    public function update($id, Request $request){
+        $usuario = Usuario::findOrFail($id);
+        $usuario->update($request->all());
+        \Session::flash('msg_update', "Atualizado com sucesso!");
+        return Redirect::to('/usuarios');
+    }
+    public function delete($id){
+        $usuario = Usuario::findOrFail($id);
+        $usuario->delete();
+        return Redirect::to('/usuarios');
     }
 }
--- resources/views/usuarios/list.blade.php
+++ resources/views/usuarios/list.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+                    <div class="card-header"><a href="{{url('usuarios/new')}}">Novo usuário</a></div>
 
                     <div class="card-body">
                         @if (session('status'))
@@ -15,9 +15,38 @@
                         @endif
 
                         <h1>Lista dos usuários</h1>
+                            <table class="table table-bordered">
+                                <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">E-mail</th>
+                                    <th scope="col">Editar</th>
+                                    <th scope="col">Deletar</th>
+                                </tr>
+                                </thead>
+                                <tbody>
                         @foreach($usuarios as $u)
-                            <p>{{$u ->name}} | {{$u->email}}</p>
+
+                                    <tr>
+                                        <th scope="row">{{$u->id}}</th>
+                                        <td>{{$u->name}}</td>
+                                        <td>{{$u->email}}</td>
+                                        <td>
+                                            <a href="usuarios/{{$u ->id}}/edit" class="btn btn-info">Editar</a>
+                                        </td>
+                                        <td>
+                                            <form action="usuarios/delete/{{$u->id}}" method="post">
+                                            @csrf
+                                                @method('delete')
+                                            <button class="btn btn-danger">Deletar</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+
                             @endforeach
+                                    </tbody>
+                                </table>
                     </div>
                 </div>
             </div>
